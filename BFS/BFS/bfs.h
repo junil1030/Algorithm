bf1e033dd22c054828fa3db80b5d350ac52d407f
Junil Seo <dccrdseo@naver.com>
2024-04-30 11:36:41 +0900

    Initial Commit: BFS

---
+++ BFS/BFS/bfs.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <list>
+#include <map>
+
+class C_BFS
+{
+private:
+	struct S_NODE
+	{
+		int nId;
+		std::list<std::map<int, S_NODE>::iterator> listIter;
+		bool bVisit;
+	};
+private:
+	std::map<int, S_NODE> m_mapNode;
+	std::map<int, S_NODE>::iterator createNode(int nId);
+
+public:
+	C_BFS() = default;
+	void add(int nDst, int nSrc);
+	void print();
+	void run();
+};
